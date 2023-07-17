14379e804491aa66f52448804bb20c55133fd08a
Thomas Vandalovsky <tvandalovsky@microsoft.com>
2023-07-17 08:56:02 -0400

    binary heap start

---
+++ src/binaryheap.rs
@@ -0,0 +1,25 @@
+
+
+pub struct BinaryHeap<T> {
+    data: Vec<T>,
+}
+
+impl BinaryHeap {
+    pub fn new() -> BinaryHeap<T> {
+        BinaryHeap {
+            data: Vec::new(),
+        }
+    }
+
+    pub fn parent(&mut self) -> Option<T> {
+
+    }
+
+    pub fn pop() -> T {
+
+    }
+
+    pub fn push(&mut self, value: T) {
+
+    }
+}
